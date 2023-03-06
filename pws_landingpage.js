604b379894cc4bcedbd2f9435b32769139154faa
Tome Mentinov <87952642+TommTo@users.noreply.github.com>
2023-03-06 02:12:37 +0100

    ., font kleiner, user input en een verhaal achrer...

--- pws_landingpage.js
+++
@@ -1,8 +0,0 @@
-var showText = function (target, message, index, interval) {   
-    if (index < message.length) {
-      $(target).append(message[index++]);
-      setTimeout(function () { showText(target, message, index, interval); }, interval);
-    }
-  }
-
-  showText("#msg", "Hello, World!", 0, 500); 
