c056b8c3292b9828434707943d1f147520ca6c63
Tome Mentinov <87952642+TommTo@users.noreply.github.com>
2023-03-06 02:10:31 +0100

    landing page begint

---
+++ pws_landingpage.js
@@ -0,0 +1,8 @@
+var showText = function (target, message, index, interval) {   
+    if (index < message.length) {
+      $(target).append(message[index++]);
+      setTimeout(function () { showText(target, message, index, interval); }, interval);
+    }
+  }
+
+  showText("#msg", "Hello, World!", 0, 500); 
